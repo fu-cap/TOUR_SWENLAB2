666b6c04f5f48b3af7790c583e4810c133fa8e60
Kacper Titowski <kacperektitow06@gmail.com>
2026-05-11 10:53:41 +0200

    Add comment to test issue linking
    #2

--- tour-planner-backend/TourPlannerAPI/Program.cs
+++ tour-planner-backend/TourPlannerAPI/Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
+// Added comment to test commit and push functionality
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
